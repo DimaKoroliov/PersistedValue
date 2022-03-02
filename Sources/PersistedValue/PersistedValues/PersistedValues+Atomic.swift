ecdb2ba42587d6d3521578861d4d2a278f384820
Dmitry Koroliov <dmitryi.koroliov@gmail.com>
2022-03-02 23:26:29 +0100

    [tech] Removed redundant code

--- Sources/PersistedValue/PersistedValues/PersistedValues+Atomic.swift
+++ Sources/PersistedValue/PersistedValues/PersistedValues+Atomic.swift
@@ -16,31 +16,17 @@ extension PersistedValues {
         public var wrappedValue: Value {
             get {
                 return self.withValue { $0 }
-//                queue.sync { self.upstream.wrappedValue }
             }
-
-//            _modify {
-//              _makeMutableAndUnique() // makes the array native, too
-//              _checkSubscript_mutating(index)
-//              let address = _buffer.mutableFirstElementAddress + index
-//              yield &address.pointee
-//              _endMutation();
-//            }
 
             nonmutating set {
                 self.withValue { _ in self.upstream.wrappedValue = newValue }
-//                queue.sync { self.upstream.wrappedValue = newValue }
             }
         }
 
-        private let queue = DispatchQueue(label: "com.dkoroliov.atomic")
         private let lock = NSLock.init()
         private let upstream: Upstream
 
         public func mutate(_ transform: (inout Upstream.Value) -> ()) {
-//            queue.sync {
-//                transform(&self.upstream.wrappedValue)
-//            }
             withValue {
                 var val = $0
                 transform(&val)
@@ -57,9 +43,6 @@ extension PersistedValues {
             defer { self.lock.unlock() }
 
             return action(self.upstream.wrappedValue)
-//            return try queue.sync {
-//                try action(self.upstream.wrappedValue)
-//            }
         }
     }
 }
